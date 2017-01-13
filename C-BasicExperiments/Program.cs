7793fc058d605410ab9fc4e6923454040e5740e8
sanczoNB <sanczo16@gmail.com>
2017-01-13 14:00:04 +0100

    Konversja z duzego inta na byte

--- C-BasicExperiments/Program.cs
+++ C-BasicExperiments/Program.cs
@@ -4,17 +4,18 @@ namespace C_BasicExperiments
 {
     class Program
     {
-        private enum Numbers:byte
+        private enum Numbers:int
         {
-           Two=1, Tree, Six=6, Seven
+           Two=1, Tree, Six=6, Seven=600
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Two to {0}", (byte)Numbers.Two);
+            Console.WriteLine("Two to {0}", (long)Numbers.Two);
             Console.WriteLine("Tree to {0}", (byte)Numbers.Tree);
             Console.WriteLine("Six to {0}", (byte)Numbers.Six);
-            Console.WriteLine("Seven to {0}", (byte)Numbers.Seven);
+            Console.WriteLine("Seven to {0}", unchecked ((byte)Numbers.Seven));
+            Console.Write(byte.MaxValue);
 
             Console.ReadKey();
         }
