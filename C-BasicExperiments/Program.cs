41d9391864f3f710d05c9f80bb9ffb00d8842f47
sanczoNB <sanczo16@gmail.com>
2017-01-13 14:02:18 +0100

    back to normal

--- C-BasicExperiments/Program.cs
+++ C-BasicExperiments/Program.cs
@@ -4,17 +4,17 @@ namespace C_BasicExperiments
 {
     class Program
     {
-        private enum Numbers:int
+        private enum Numbers:byte
         {
-           Two=1, Tree, Six=6, Seven=600
+           Two=1, Tree, Six=6, Seven=7
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Two to {0}", (long)Numbers.Two);
+            Console.WriteLine("Two to {0}", (byte)Numbers.Two);
             Console.WriteLine("Tree to {0}", (byte)Numbers.Tree);
             Console.WriteLine("Six to {0}", (byte)Numbers.Six);
-            Console.WriteLine("Seven to {0}", unchecked ((byte)Numbers.Seven));
+            Console.WriteLine("Seven to {0}", (byte)Numbers.Seven);
             Console.Write(byte.MaxValue);
 
             Console.ReadKey();
