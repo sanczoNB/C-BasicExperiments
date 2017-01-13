1375610c2a62128248054acd6fdf81b1a9cf293e
sanczoNB <sanczo16@gmail.com>
2017-01-13 13:51:36 +0100

    Jak zachowuje się inkrementacja enumów

--- C-BasicExperiments/Program.cs
+++ C-BasicExperiments/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace C_BasicExperiments
+﻿using System;
+
+namespace C_BasicExperiments
 {
     class Program
     {
+        private enum Numbers:byte
+        {
+           Two=1, Tree, Six=6, Seven
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Two to {0}", (byte)Numbers.Two);
+            Console.WriteLine("Tree to {0}", (byte)Numbers.Tree);
+            Console.WriteLine("Six to {0}", (byte)Numbers.Six);
+            Console.WriteLine("Seven to {0}", (byte)Numbers.Seven);
+
+            Console.ReadKey();
         }
     }
 }
